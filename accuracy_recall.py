063b9e7cb62fb368472e2ef88172c49b07cb154d
kushvikas <65815229+kushvikas@users.noreply.github.com>
2021-03-27 22:18:34 +0100

    Update accuracy_recall.py

--- accuracy_recall.py
+++ accuracy_recall.py
@@ -31,3 +31,20 @@ print(precision)
 
 f_1 = 2 * (precision * recall) / (precision + recall)
 print(f_1)
+
+
+
+
+
+_____________________________________________________________________________________________________________________________________________________________________________
+
+
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+
+labels = [1, 0, 0, 1, 1, 1, 0, 1, 1, 1]
+guesses = [0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
+
+print(accuracy_score(labels, guesses))
+print(recall_score(labels, guesses))
+print(precision_score(labels, guesses))
+print(f1_score(labels, guesses))
